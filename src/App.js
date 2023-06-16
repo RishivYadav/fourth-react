e1e939aad52594122c10f37b57700e0be4b66630
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-16 17:33:02 +0530

    multiple box

--- src/App.js
+++ src/App.js
@@ -3,74 +3,42 @@ import { useRef, useState } from "react";
 function App() {
   return (
     <>
-      <h1>My Project</h1>
-      <ListDemo />
+      <h1>My Todo</h1>
+      <MyTodo />
     </>
   );
 }
 
-function ListDemo() {
-  let inputRef = useRef();
-  let [list, setList] = useState(["delhi"]);
+function MyTodo() {
+  // let todo = { task: "" };
+  // S1 :: FORM :: MULTIPLE INPUT FIELD
+  let [todo, setTodo] = useState({ task: "" });
 
-  let addItemAction = () => {
-    let inputValue = inputRef.current.value;
-    let newList = [inputValue, ...list];
-    setList(newList);
+  // S3 :: gething the input object on chagne
+  let handleChnageTaskAction = (e) => {
+    console.log(e.target);
+    // e.target === input object
 
-    inputRef.current.value = "";
+    let newTodo = { ...todo, task: e.target.value };
+    setTodo(newTodo);
+  };
+
+  // S4 :: We will be making API call.
+  let addTodoAction = () => {
+    alert(todo.task);
   };
 
   return (
     <>
-      <div
-        className="row justify-content-center align-items-center"
-        style={{ height: "100vh" }}
-      >
-        <div className="col-sm-12 col-md-6">
-          <h1>Registraton App</h1>
-          <input
-            className="form-control"
-            type="text"
-            id="id1"
-            ref={inputRef}
-            placeholder="Enter user input..."
-          />
-          <input
-            className="form-control"
-            type="text"
-            id="id1"
-            ref={inputRef}
-            placeholder="Enter Password..."
-          />
-          <input
-            className="form-control"
-            type="text"
-            id="id1"
-            ref={inputRef}
-            placeholder="Enter Email..."
-          />
-          <input type="button" value="Login" onClick={addItemAction} />
-        </div>
-      </div>
-    </>
-  );
-}
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Enter task"
+        value={todo.task}
+        onChange={handleChnageTaskAction}
+      />
 
-function MessageDemo({ message }) {
-  return (
-    <>
-      <h1>Hello {message}</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-        dolorem quas sequi tenetur quo odit cupiditate assumenda voluptas
-        quibusdam blanditiis voluptatem quis mollitia eaque molestiae animi,
-        provident eligendi facilis incidunt.
-      </p>
-      <div>
-        <input type="button" value="&#128077;" />
-        <input type="button" value="&#128078;" />
-      </div>
+      <input type="button" value="Add Todo" onClick={addTodoAction} />
     </>
   );
 }
